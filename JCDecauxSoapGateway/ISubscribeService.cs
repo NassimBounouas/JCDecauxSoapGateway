5f2bc4b389ca7b7da0281495644c20b277b0554a
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2018-04-05 09:53:51 +0200

    Event interfaces

---
+++ JCDecauxSoapGateway/ISubscribeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace JCDecauxSoapGateway
+{
+    [ServiceContract(CallbackContract = typeof(ISubscribeServiceEvents))]
+    public interface ISubscribeService
+    {
+        [OperationContract]
+        void Subscribe(Contract c, int time);
+    }
+}
---
+++ JCDecauxSoapGateway/ISubscribeServiceEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace JCDecauxSoapGateway
+{
+    interface ISubscribeServiceEvents
+    {
+        [OperationContract(IsOneWay = true)]
+        void Subscribe(Contract c, int seconds);
+    }
+}
